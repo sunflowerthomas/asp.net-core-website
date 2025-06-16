cb8a4a32c2b060e5ada95b36e52ba559b8c73d63
sunflowerthomas <t.man.sunflower@gmail.com>
2025-06-16 20:05:31 +0100

    added guestbook

---
+++ Pages/Guestbook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp.net_core_website.Pages
+{
+    public class GuestbookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
